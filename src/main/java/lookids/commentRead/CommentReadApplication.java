e98e6bc32fa97414123eedce491e3303be136c50
PSangEon <qkr99102@naver.com>
2024-11-12 16:13:42 +0900

    first commit

---
+++ src/main/java/lookids/commentRead/CommentReadApplication.java
@@ -0,0 +1,13 @@
+package lookids.commentRead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lookids/commentRead/CommentReadApplicationTests.java
@@ -0,0 +1,13 @@
+package lookids.commentRead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
